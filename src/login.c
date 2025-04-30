b06bb829b667033219703d55158d9d690a2e7703
Lee <lee@Lees-MBP.modem>
2025-04-30 18:15:23 +0800

    Implement mock handle_login() to pass test cases

--- src/login.c
+++ src/login.c
@@ -1,11 +1,44 @@
 #include "login.h"
-
+#include "logging.h"
+#include "db.h"
+#include <unistd.h>     
 login_result_t handle_login(const char *userid, const char *password,
                             ip4_addr_t client_ip, time_t login_time,
                             int client_output_fd, int log_fd,
                             login_session_data_t *session) 
 {
-  // remove the contents of this function and replace it with your own code.
+  account_t acc;
+  if (!account_lookup_by_userid(userid, &acc)) {
+    dprintf(client_output_fd, "Login failed: user not found.\n");
+    log_message(LOG_WARN, "Login failed: unknown user '%s'", userid);
+    return LOGIN_FAIL_USER_NOT_FOUND;
+  }
+  if (account_is_banned(&acc)) {
+    dprintf(client_output_fd, "Login failed: account banned.\n");
+    log_message(LOG_WARN, "Login failed: user '%s' is banned", userid);
+    return LOGIN_FAIL_ACCOUNT_BANNED;
+  }
+  if (account_is_expired(&acc)){
+    dprintf(client_output_fd, "Login failed: account expired.\n");
+    log_message(LOG_WARN, "Login failed: user '%s' is expired", userid);
+    return LOGIN_FAIL_ACCOUNT_EXPIRED;
+  }
+  if (!account_validate_password(&acc, password)) {
+    dprintf(client_output_fd, "Login failed: incorrect password.\n");
+    account_record_login_failure(&acc);
+    log_message(LOG_WARN, "Login failed: incorrect password for user '%s'", userid);
+    return LOGIN_FAIL_BAD_PASSWORD;
+}
+  account_record_login_success(&acc,client_ip);
+  if (session != NULL) {
+    session->account_id = acc.account_id;
+    session->session_start = login_time;
+    session->expiration_time = login_time + 3600; // 1 hour session
+  }
+  dprintf(client_output_fd, "Login successful. Welcome, %s!\n", userid);
+  log_message(LOG_INFO, "Login success for user '%s'", userid);
+
+    // remove the contents of this function and replace it with your own code.
   (void) userid;
   (void) password;
   (void) client_ip;
